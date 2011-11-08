4232b75dd724b34cb13c07d7de6810c592d6c756
Taufiq Hoven <taufiq.hoven@gmail.com>
2011-11-08 20:42:02 +1000

    Added ad-hoc testing functionality.

--- src/main/java/com/quiptiq/carrit/WorldFileLoader.java
+++ src/main/java/com/quiptiq/carrit/WorldFileLoader.java
@@ -15,7 +15,29 @@ public class WorldFileLoader {
      *            Name of the map.
      * @return ChunkWorld at the specified map, or null if non exists.
      */
-    public static ChunkWorld LoadWorld(String mapName) {
+    public static ChunkWorld loadWorld(String mapName) {
         return new chunk_world().loadChunkWorld(mapName);
+    }
+
+    public static void main(String [] args) {
+        if (args.length == 1) {
+            ChunkWorld world = loadWorld(args[0]);
+            System.out.println("Has chunk? " + world.hasChunk(0, 0, 0));
+            Chunk testChunk = world.getChunk(0, 0, 0);
+
+            int numBytes = world.getChunkSizeX() * world.getChunkSizeY() * world.getChunkSizeZ();
+            System.out.println("Got chunk, " + numBytes + ":");
+
+            byte[] blockIds = testChunk.getBlockIds();
+            for (int x = 0; x < world.getChunkSizeX(); x++) {
+                for (int z = 0; z < world.getChunkSizeZ(); z++) {
+                    StringBuilder builder = new StringBuilder(world.getChunkSizeY());
+                    for (int y = 0; y < world.getChunkSizeY(); y++) {
+                        builder.append(blockIds[y + z * world.getChunkSizeY() + x * world.getChunkSizeY() * world.getChunkSizeZ() ]);
+                    }
+                    System.out.println(x + "," + z + ":" + builder.toString());
+                }
+            }
+        }
     }
 }
